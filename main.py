392a912d90daa01aa79633f543dbf8b4a1a3ff16
ronan tremoureux <ronan.tremoureux@gmail.com>
2024-01-02 12:04:15 +0100

    getting the clan name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cocapi import Player
+from cocapi import Clan
 from cocapi import loadToken
 from cocapi import clanService
 #this code will retrieve all the names and tags of all the player in the same clan as luminawight.
@@ -7,7 +8,10 @@ myClanService = clanService()
 
 loadToken("TOKEN") # this is the path of a file you've locally created containing you api token
 
-player = Player("#VPJY8CVG") # tag of player luminawight
+player = Player("#VPJY8CVG") # tag of player luminawight 
+clan = Clan(player.getClanTag())
+
+print("Members of clan : " + clan.getInfos().get("name"))
 
 for member in myClanService.getMembersOfClan(player.getClanTag(),True):
     print(member.getInfos().get("name"),member.getInfos().get("tag"))
