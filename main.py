7415df39ac8ab75da2476769ba551dd1af2fb990
ronan tremoureux <ronan.tremoureux@gmail.com>
2024-01-02 12:00:18 +0100

    following latest version of the lib

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from cocapi import Player
 from cocapi import loadToken
+from cocapi import clanService
+#this code will retrieve all the names and tags of all the player in the same clan as luminawight.
 
-loadToken("TOKEN") # this is the path of a file you locately created containing you api token
+myClanService = clanService()
+
+loadToken("TOKEN") # this is the path of a file you've locally created containing you api token
 
 player = Player("#VPJY8CVG") # tag of player luminawight
 
-members = player.getClan().getMembers()
-playerlist = []
+for member in myClanService.getMembersOfClan(player.getClanTag(),True):
+    print(member.getInfos().get("name"),member.getInfos().get("tag"))
 
-for member in members:
-    playerlist.append( member.get("tag"))
-
-print(playerlist)
+if player.verify("game-api-token"): # this token can be found in the game parameters under "API TOKEN", usefull to authanticate an account that exists
+    print("Player verified " + player.global_info.get("name", "No Name"))
+else:
+    print("Player not verified")
